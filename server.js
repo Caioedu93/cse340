ad63b456aff02ca39b5b72bb0e1297752aefe387
Caioededu93 <depaulacaio10@gmail.com>
2025-03-19 16:44:09 -0300

    week3 activity

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
+const baseController = require("./controllers/baseController")
 
 
 /* ***********************
@@ -28,9 +30,10 @@ app.use(static);
 /* ***********************
  * Index route
  *************************/
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
